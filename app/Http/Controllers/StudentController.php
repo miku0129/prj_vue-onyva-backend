aeebdf21962bd033900138c368e00051990fbec6
miku0129 <svartkatt39@gmail.com>
2024-07-09 11:22:00 +0200

    feat: add POST method

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,10 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Student; 
+
+use Validator;
+
 class StudentController extends Controller
 {
     public function index()
     {
-        return "i am from api controller"; 
+        $student = Student::all(); 
+
+        $data = [
+            'status'=>200, 
+            'student'=>$student
+        ]; 
+
+        return response()->json($data, 200); 
+    }
+
+    public function upload(Request $request)
+    {
+        $validator = Validator::make($request->all(), 
+        [
+            'name'=>'required',
+            'email'=>'required|email'
+        ]);
+
+        if($validator->fails())
+        {
+            $data=[
+                'status'=>422, 
+                'message'=>$validator->messages()
+            ];
+            return response()->json($data, 422); 
+        }
+
+        else 
+        {
+            $student = new Student; 
+
+            $student->name=$request->name; 
+            $student->email=$request->email; 
+            $student->phone=$request->phone; 
+
+            $student->save(); 
+
+            $data=[
+                'status'=>200, 
+                'message'=>'Data uploaded successfully'
+            ];
+
+            return response()->json($data, 200);
+
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -21,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('student', [StudentController::class,'index']);
 
+Route::post('student', [StudentController::class,'upload']);
